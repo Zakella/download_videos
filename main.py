584a93282f7c6f83404d61bf6c9e64a4785c81db
Zakella <zapolski.slava@gmail.com>
2022-05-20 11:38:28 +0300

    Создание папки без ошибки

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 import urllib.request
-import os
+from pathlib import Path
 
 name = input("file name: ")
 path = input("folder : ")
 url = input("url: ")
 print(f"Start downloading {name}")
-os.makedirs(path, exist_ok=False)
+Path(path).mkdir(parents=True, exist_ok=True)
 urllib.request.urlretrieve(url, f"{path}/{name}")
-print(f"End! {name}")
+print(f"End! {Path(path).absolute()}/{name}")
+
 
