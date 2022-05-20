7c987e8c1b5a8b065fdc7ad89fae269d8e0586d6
Zakella <zapolski.slava@gmail.com>
2022-05-20 11:12:47 +0300

    добалено скачивание видео

--- main.py
+++ main.py
@@ -1,16 +1,11 @@
-# This is a sample Python script.
+import urllib.request
+import os
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+name = input("file name: ")
+path = input("folder : ")
+url = input("url: ")
+print(f"Start downloading {name}")
+os.makedirs(path, exist_ok=False)
+urllib.request.urlretrieve(url, f"{path}/{name}")
+print(f"End! {name}")
 
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
