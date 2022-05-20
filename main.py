70cfa1440c4d697854bbeff8db44f81d2b8f8e7c
Zakella <zapolski.slava@gmail.com>
2022-05-20 11:39:20 +0300

    Создание папки без ошибки 1

--- main.py
+++ main.py
@@ -7,6 +7,6 @@ url = input("url: ")
 print(f"Start downloading {name}")
 Path(path).mkdir(parents=True, exist_ok=True)
 urllib.request.urlretrieve(url, f"{path}/{name}")
-print(f"End! {Path(path).absolute()}/{name}")
+print(f"End! {Path(path).absolute()}\{name}")
 
 
